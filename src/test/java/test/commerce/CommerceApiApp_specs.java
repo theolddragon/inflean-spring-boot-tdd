2b96500fd7d0bfbd4a62ab3b29a0a828da09ff5b
Gyuwon Yi <gyuwon@live.com>
2024-10-15 22:50:00 +0900

    API 앱 프로젝트를 초기화한다

---
+++ src/test/java/test/commerce/CommerceApiApp_specs.java
@@ -0,0 +1,13 @@
+package test.commerce;
+
+import commerce.CommerceApiApp;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CommerceApiApp.class)
+public class CommerceApiApp_specs {
+
+    @Test
+    void 컨텍스트가_초기화된다() {
+    }
+}
