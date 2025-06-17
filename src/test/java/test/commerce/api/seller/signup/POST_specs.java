9438f8a177f71b86db95c75e7a91acf7ea453b5f
박동욱 <wanderhoward@gmail.com>
2025-06-17 14:35:32 +0900

    p-26

--- src/test/java/test/commerce/api/seller/signup/POST_specs.java
+++ src/test/java/test/commerce/api/seller/signup/POST_specs.java
@@ -1,8 +1,15 @@
 package test.commerce.api.seller.signup;
 
 import commerce.CommerceApiApp;
+import commerce.command.CreateSellerCommand;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
     classes = CommerceApiApp.class,
@@ -10,4 +17,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 )
 @DisplayName("POST /seller/signUp")
 public class POST_specs {
+    @Test
+    void 올바르게_요청하면_204_No_Content_상태코드를_반환한다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            "seller1@test.com",
+            "seller",
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
 }
