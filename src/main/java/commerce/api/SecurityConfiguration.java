069a8f07aabbfe6f8a1d46c5cdc6f411f07e716f
박동욱 <wanderhoward@gmail.com>
2025-06-17 17:12:39 +0900

    p-38

---
+++ src/main/java/commerce/api/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package commerce.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    Pbkdf2PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+}
