8a2038d9b4a58985286709191bf00531e3b186c4
박동욱 <wanderhoward@gmail.com>
2025-06-18 15:57:22 +0900

    p-42

--- src/main/java/commerce/api/controller/SellerIssueTokenController.java
+++ src/main/java/commerce/api/controller/SellerIssueTokenController.java
@@ -1,13 +1,24 @@
 package commerce.api.controller;
 
 import commerce.result.AccessTokenCarrier;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.spec.SecretKeySpec;
+
 @RestController
-public record SellerIssueTokenController() {
+public record SellerIssueTokenController(
+    @Value("${security.jwt.secret}") String jwtSecret
+) {
     @PostMapping("/seller/issueToken")
     AccessTokenCarrier issueToken() {
-        return new AccessTokenCarrier("token");
+        return new AccessTokenCarrier(
+            Jwts
+                .builder()
+                .signWith(new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256"))
+                .compact()
+        );
     }
 }
--- src/test/java/test/commerce/api/seller/issuetoken/POST_specs.java
+++ src/test/java/test/commerce/api/seller/issuetoken/POST_specs.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import test.commerce.JwtAssertions;
 
+import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static test.commerce.EmailGenerator.generateEmail;
+import static test.commerce.JwtAssertions.conformsToJwtFormat;
 import static test.commerce.PasswordGenerator.generatePassword;
 import static test.commerce.UsernameGenerator.generateUsername;
 
@@ -70,5 +73,31 @@ public class POST_specs {
         // Assert
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().accessToken()).isNotNull();
+    }
+
+    @Test
+    void 접근_토큰은_JWT_형식을_따른다(
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        String email = generateEmail();
+        String password = generatePassword();
+
+        client.postForEntity(
+            "/seller/signUp",
+            new CreateSellerCommand(email, generateUsername(), password),
+            Void.class
+        );
+
+        // Act
+        ResponseEntity<AccessTokenCarrier> response = client.postForEntity(
+            "/seller/issueToken",
+            new IssueSellerToken(email, password),
+            AccessTokenCarrier.class
+        );
+
+        // Assert
+        String actual = requireNonNull(response.getBody()).accessToken();
+        assertThat(actual).satisfies(conformsToJwtFormat());
     }
 }
