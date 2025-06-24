b442032d0f523505075d0ad1d4750be26f3f9236
박동욱 <wanderhoward@gmail.com>
2025-06-24 16:07:21 +0900

    p-80

--- src/main/java/commerce/Product.java
+++ src/main/java/commerce/Product.java
@@ -9,12 +9,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(indexes = @Index(columnList = "sellerId"))
 public class Product {
 
     @Id
